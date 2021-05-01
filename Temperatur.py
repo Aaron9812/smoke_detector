899e9a7c1e4a5592bb642d407f6935fe13841942
tdmauch <82529222+tdmauch@users.noreply.github.com>
2021-05-01 13:54:29 +0200

    Add files via upload
    Temperaturmessung für smoke detector

---
+++ Temperatur.py
@@ -0,0 +1,39 @@
+# smarter Feuermelder IOX CODING IOT
+
+# Temperaturabfrage
+import os, sys, time, datetime
+
+
+def aktuelleTemperatur():
+      
+    # 1-wire Slave Datei lesen
+    file = open('/sys/bus/w1/devices/28-3c01d607c29f/w1_slave')
+    filecontent = file.read()
+    file.close()
+
+    # Temperaturwerte auslesen und konvertieren
+    stringvalue = filecontent.split("\n")[1].split(" ")[9] #Abfrage des Wertes in Zeile [1] an der Stelle [9]. Hier befindet sich die Temp
+    temperature = float(stringvalue[2:]) / 1000 # gibt Wert ab 3. Stelle an, da Wert als "t=23765" angegeben wird
+
+    # Temperatur ausgeben
+    rueckgabewert = '%6.2f' % temperature # %6 -->Dezimalzahl wird auf 6 Stellen mit Nullen aufgefüllt, um Kommafehler bei 1-stelligen Celsiuswerten zu vermeiden. 2:--> Komma nach zwei Stellen
+    return(rueckgabewert)
+
+schleifenZaehler = 0
+Datum = datetime.datetime.now()
+schleifenPause = 1
+
+# Ausgabe zu Messbeginn
+print ("Temperaturabfrage für ", Datum,
+      " Messungen alle ", schleifenPause ," Sekunden gestartet")
+
+# Ausgabe während der Messung
+while True:
+    messdaten = aktuelleTemperatur()
+    Datum = datetime.datetime.now() #aktueller Zeitstempel
+    print ("Aktuelle Temperatur : ", messdaten, "°C",
+    "in der ", schleifenZaehler, ". Messabfrage", "Zeitstempel: ", Datum)
+    time.sleep(schleifenPause)
+    schleifenZaehler = schleifenZaehler + 1
+    
+
