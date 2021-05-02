d9a31b3643285ab714434720a9040f00b6605dc3
Aaron9812 <79525803+Aaron9812@users.noreply.github.com>
2021-05-02 21:20:38 +0200

    Integrated dash in Flask

--- dashboard.py
+++ dashboard.py
@@ -10,11 +10,57 @@ import api
 
 from datetime import date
 
-# external CSS stylesheets
-external_stylesheets = ["static\dashboard.css"]
-    
+def init_dashboard(server):
+    #creating a dash Dashboard
+    dash_app = dash.Dash(
+        server=server,
+        requests_pathname_prefix="/dashapp/",
+        external_stylesheets=[
+            "/static/css/dashboard.css"
+        ]
+    )    
 
-app = dash.Dash(__name__)
+    df = clean_Data()
+
+    #creating the dash layout 
+    dash_app.layout = html.Div([
+        html.H1("IOT Dashboard"),
+
+        dash_core_components.DatePickerRange(
+            id="my-date-picker-range",
+            min_date_allowed=date(2021, 4, 17),
+            max_date_allowed=date(2021, 12, 31),
+            start_date=date.today(),
+            end_date=date(2021, 8, 25)
+        ),
+        
+        html.Br(),
+
+        dash_core_components.Graph(id="temp-chart"),
+        dash_core_components.Graph(id="humidity-chart")
+    ])
+    return dash_app.server
+
+def init_callbacks(dash_app):
+    @app.callback(
+        [Output(component_id="temp-chart", component_property="figure"),
+        Output(component_id="humidity-chart", component_property="figure")],
+        [Input(component_id="my-date-picker-range", component_property="start_date"),
+        Input(component_id="my-date-picker-range", component_property="end_date")]
+    )
+
+    def update_graph(start_date, end_date):
+        
+        dff_filtered = dff.loc[start_date : end_date]
+        
+        fig_1 = px.line(dff_filtered, 
+            x="timestamp", y="temperature")
+        fig_2 = px.line(dff_filtered, 
+            x="timestamp", y="humidity")
+
+        return fig_1 , fig_2
+
+
 
 def clean_Data():
     con = api.connect_to_DB()
@@ -26,40 +72,6 @@ def clean_Data():
     df.sort_index(inplace=True, ascending=True)
     return df
 
-app.layout = html.Div([
-    html.H1("IOT Dashboard"),
-
-    dash_core_components.DatePickerRange(
-        id="my-date-picker-range",
-        min_date_allowed=date(2021, 4, 17),
-        max_date_allowed=date(2021, 12, 31),
-        start_date=date.today(),
-        end_date=date(2021, 8, 25)
-    ),
-    
-    html.Br(),
-
-    dash_core_components.Graph(id="temp-chart"),
-    dash_core_components.Graph(id="humidity-chart")
-])
-
-@app.callback(
-    [Output(component_id="temp-chart", component_property="figure"),
-    Output(component_id="humidity-chart", component_property="figure")],
-    [Input(component_id="my-date-picker-range", component_property="start_date"),
-    Input(component_id="my-date-picker-range", component_property="end_date")]
-)
-
-def update_graph(start_date, end_date):
-    
-    dff_filtered = dff.loc[start_date : end_date]
-    
-    fig_1 = px.line(dff_filtered, 
-        x="timestamp", y="temperature")
-    fig_2 = px.line(dff_filtered, 
-        x="timestamp", y="humidity")
-
-    return fig_1 , fig_2
 
 if __name__ == "__main__":
     dff = clean_Data()
