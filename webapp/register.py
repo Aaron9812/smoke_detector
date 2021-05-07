f90f2b3349463fbf52f870ce9fd3f0f71128b952
PDamg <65674032+PDamg@users.noreply.github.com>
2021-05-07 15:40:06 +0200

    register

---
+++ webapp/register.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template,redirect
+import api
+import os
+import re
+import mariadb
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+	if request.method == 'POST':
+		first_name = request.form['first_name']
+		last_name = request.form['last_name']
+		email = request.form['email']
+		phone_no = request.form['phone_no']
+		password = request.form['password']
+		street = request.form['street']
+		street_no = request.form['street_no']
+		zipcode = request.form['zipcode']
+		city = request.form['city']
+
+		con = api.connect_to_DB()
+		cursor = con.cursor()
+
+		query1 = f"INSERT INTO Test_User (first_name, last_name, email, phone_no, password, street, street_no, zipcode, city) VALUES ('{first_name}', '{last_name}', '{email}', '{phone_no}', '{password}', '{street}', '{street_no}', {zipcode}, '{city}')"
+
+		cursor.execute(query1)
+		con.commit()
+		con.close()
+
+
+		return redirect("/")
+
+	return render_template('register.html')
+
+if __name__ == "__main__":
+	app.run(debug=True)
